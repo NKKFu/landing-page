f314205de75871e246b2a56fa0d652e8055f2aff
nkkf <nelsonkenmochi@gmail.com>
2020-05-13 21:23:03 -0400

    navbar edited
    Estou me inspirando no navbar da google.org , por enquanto vou deixar igual.

--- HTML/javascript/navbar.js
+++ HTML/javascript/navbar.js
@@ -2,18 +2,18 @@
 let lastScroll = 0;
 
 window.onscroll = function () {
-    if (window.scrollY < 10) {
-        document.getElementById("navbar").style.top = "0";
+    if (window.scrollY < 50) {
+        document.getElementById("navbar").classList.remove('header-off');
         return;
     }
 
-    if (lastScroll < window.scrollY) {
+    if (lastScroll > window.scrollY) {
         // appears
-        document.getElementById("navbar").style.top = "0";
+        document.getElementById("navbar").classList.remove('header-off');
         lastScroll = window.scrollY;
-    } else if (lastScroll > window.scrollY) {
+    } else if (lastScroll < window.scrollY) {
         // disappears
-        document.getElementById("navbar").style.top = "-50px";
+        document.getElementById("navbar").classList.add('header-off');
         lastScroll = window.scrollY;
     }
 };
